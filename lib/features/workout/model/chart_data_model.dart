047eb1d2f0c53b1c3eabda3ce77b7a3c534d7da3
zaihcodes <zaihwork@gmail.com>
2024-07-03 11:53:26 +0100

    add some data for the chart

---
+++ lib/features/workout/model/chart_data_model.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+
+class ChartDataModel {
+  final String title;
+  final double percent;
+  final Color color;
+
+  ChartDataModel(
+      {required this.title, required this.percent, required this.color});
+}
